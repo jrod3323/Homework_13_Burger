a684144cec3fe951d1071f099a447dadd9360370
Jared Ackley <jrod3323@gmail.com>
2021-01-19 13:42:43 -0600

    add burger.all to call orm.selectAll

--- config/orm.js
+++ config/orm.js
@@ -7,26 +7,27 @@ var connection = require("./connection.js");
 // These help avoid SQL injection
 // https://en.wikipedia.org/wiki/SQL_injection
 var orm = {
-    selectAll: function() {
+    selectAll: function(cb) {
         var queryString = "SELECT * FROM burgers_db.burgers";
         connection.query(queryString, function(err, result) {
           if (err) throw err;
-          console.log(result);
+          cb(result);
         });
       },
-      insertOne: function(burger_name) {
+      insertOne: function(burger_name,cb) {
         var queryString = "INSERT INTO burgers (burger_name) Values(?);";
         connection.query(queryString, [burger_name], function(err, result) {
           if (err) throw err;
-          console.log(result);
+          cb(result);
         });
       },
-      updateOne: function(devoured,id) {
+      updateOne: function(devoured,id,cb) {
         var queryString = "Update burgers_db.burgers SET devoured = ? where id =?;";
         connection.query(queryString, [devoured,id], function(err, result) {
           if (err) throw err;
-          console.log(result);
+          cb(result);
         });
+    }
 };
 
 module.exports = orm;
