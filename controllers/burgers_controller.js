e06624444700c72c31cfb8e0a394dc0c1c0226a4
Jared Ackley <jrod3323@gmail.com>
2021-01-19 14:10:54 -0600

    add .get request to contollers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,5 +5,16 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burgers.all(function(data) {
+      var dataObject = {
+        burgers: data
+      };
+      console.log(dataObject);
+      res.render("index", dataObject);
+    });
+  });
+
+
 // Export routes for server.js to use.
 module.exports = router;
