7b6479ce9d3d3fbae642286afe7c344d4ca295f6
Jared Ackley <jrod3323@gmail.com>
2021-01-19 13:49:14 -0600

    export router set up

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,6 @@ var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
+
+// Export routes for server.js to use.
+module.exports = router;
