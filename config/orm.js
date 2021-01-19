e763ea266665670df123b8fb9544f5b4dee4faf3
Jared Ackley <jrod3323@gmail.com>
2021-01-19 13:31:06 -0600

    add quereies selectAll and insertOne to orm.js

--- config/orm.js
+++ config/orm.js
@@ -7,7 +7,20 @@ var connection = require("./connection.js");
 // These help avoid SQL injection
 // https://en.wikipedia.org/wiki/SQL_injection
 var orm = {
-  
+    selectAll: function() {
+        var queryString = "SELECT * FROM burgers_db.burgers";
+        connection.query(queryString, function(err, result) {
+          if (err) throw err;
+          console.log(result);
+        });
+      },
+      insertOne: function(burger_name) {
+        var queryString = "INSERT INTO burgers (burger_name) Values(?);";
+        connection.query(queryString, [burger_name], function(err, result) {
+          if (err) throw err;
+          console.log(result);
+        });
+      },
 };
 
 module.exports = orm;
