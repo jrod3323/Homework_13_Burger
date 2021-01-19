744da5f769a6b640db7d8b99648f942144cd9b90
Jared Ackley <jrod3323@gmail.com>
2021-01-19 13:17:39 -0600

    add db file and schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE cat_db;
+USE cat_db;
+
+CREATE TABLE cats
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
