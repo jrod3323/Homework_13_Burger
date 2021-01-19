cbdf3ddc6d1c79dc991001dd130d56cc60b0b934
Jared Ackley <jrod3323@gmail.com>
2021-01-19 13:19:46 -0600

    add seeds.sql file with queries to insert 3 burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) Values("Impossible Burger");
+INSERT INTO burgers (burger_name) Values("Beyond Burger");
+INSERT INTO burgers (burger_name) Values("California Burger");
