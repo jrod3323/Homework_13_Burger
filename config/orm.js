d7c5533fb1221c8a482c5a65d0b53cb26cbfd79b
Jared Ackley <jrod3323@gmail.com>
2021-01-19 13:27:08 -0600

    add orm.js with connection.js requred and orm exported

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+var connection = require("./connection.js");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+var orm = {
+  
+};
+
+module.exports = orm;
